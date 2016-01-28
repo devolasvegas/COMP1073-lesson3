ebea9cfc2ba8c6f0211c2a9538aab1636fd1b2d9
DEVON DAVIAU <devon.daviau@yahoo.ca>
2016-01-28 10:51:13 -0500

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,14 @@
+// main javascript document
+
+"use strict";
+
+var username = "Devon";
+var age = 34;
+var isHungry = true;
+
+var usernames = [];
+//var usernames = new Array();
+
+usernames[0] = "Mike";
+
+console.log(usernames);
